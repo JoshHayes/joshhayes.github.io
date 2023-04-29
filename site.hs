5ed05b452b39fd6064a3f7bdde720e08299d3acc
Joshua Hayes <joshjohayes@gmail.com>
2023-04-29 21:59:29 +0200

    assorted

--- site.hs
+++ site.hs
@@ -4,21 +4,32 @@
 import Hakyll
 
 --------------------------------------------------------------------------------
-config :: Configuration
-config =
-  defaultConfiguration
-    { destinationDirectory = "docs"
-    }
 
 main :: IO ()
 main = hakyllWith config $ do
-  match "images/*" $ do
-    route idRoute
-    compile copyFileCompiler
+  match "templates/*" $ compile templateCompiler
 
   match "css/*" $ do
     route idRoute
     compile compressCssCompiler
+
+  match "images/*" $ do
+    route idRoute
+    compile copyFileCompiler
+
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/**"
+      let indexCtx =
+            listField "posts" postCtx (pure posts)
+              <> constField "title" "Home"
+              <> defaultContext
+
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" indexCtx
+        >>= relativizeUrls
 
   match (fromList ["about.md", "contact.md"]) $ do
     route $ setExtension "html"
@@ -40,40 +51,26 @@ main = hakyllWith config $ do
     compile $ do
       posts <- recentFirst =<< loadAll "posts/*"
       let archiveCtx =
-            listField "posts" postCtx (return posts)
-              `mappend` constField "title" "Archives"
-              `mappend` defaultContext
+            listField "posts" postCtx (pure posts)
+              <> constField "title" "Archives"
+              <> defaultContext
 
       makeItem ""
         >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
         >>= loadAndApplyTemplate "templates/default.html" archiveCtx
         >>= relativizeUrls
 
-  match "index.html" $ do
-    route idRoute
-    compile $ do
-      posts <- recentFirst =<< loadAll "posts/*"
-      let indexCtx =
-            listField "posts" postCtx (return posts)
-              `mappend` constField "title" "Home"
-              `mappend` defaultContext
+--------------------------------------------------------------------------------
+-- Config
 
-      getResourceBody
-        >>= applyAsTemplate indexCtx
-        >>= loadAndApplyTemplate "templates/default.html" indexCtx
-        >>= relativizeUrls
-
-  match "templates/*" $ compile templateCompiler
+config :: Configuration
+config =
+  defaultConfiguration
+    { destinationDirectory = "docs"
+    }
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
   dateField "date" "%B %e, %Y"
-    `mappend` defaultContext
-
--- >>> unContext defaultContext
--- No instance for (Show
---                    (String -> [String] -> Item String -> Compiler ContextField))
---   arising from a use of `evalPrint'
---   (maybe you haven't applied a function to enough arguments?)
--- In a stmt of an interactive GHCi command: evalPrint it_adCQ
+    <> defaultContext
